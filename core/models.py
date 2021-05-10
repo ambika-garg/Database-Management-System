26db3150cdde79bcb0c017c87acc5aaad0b39d9b
IshitaArora-1010 <ishita.arora2001@gmail.com>
2021-05-10 16:23:05 +0530

    Models update

--- core/models.py
+++ core/models.py
@@ -13,33 +13,11 @@ from collectionfield.models import CollectionField
 class category(models.Model):
     category_name = models.CharField(max_length=100)
 
-class AddressField(CompositeField):
-    state = models.CharField(max_length=30)
-    location = models.CharField(max_length=50, unique=False)
-    city = models.CharField(max_length=40, unique=False)
-
-class NameField(CompositeField):
-    salutation = models.CharField(max_length=25, blank=True)
-    FirstName = models.CharField(max_length=25, blank=True)
-    LastName = models.CharField(max_length=25, blank=True)
-
-class ParticipantComAddField(CompositeField):
-    address_comm = models.CharField(max_length=100, blank=True)
-    state_comm = models.CharField(max_length=25, blank=True)
-    district_comm = models.CharField(max_length=25, blank=True)
-    pincode_comm = models.IntegerField()
-    city_comm = models.CharField(max_length=25, blank=True)
-    tehsil_comm = models.CharField(max_length=25, blank=True)
-    constituency_comm = models.CharField(max_length=25, blank=True)
-
-
 class dept_entre(models.Model):
     category_ent = models.ForeignKey(category, default=1, verbose_name="category", on_delete=models.CASCADE)
-    department_name = models.CharField(max_length=100)
-
+    department_nam_ent = models.CharField(max_length=100)
     def __str__(self):
         return self.department_name
-
 
 class dept_skill(models.Model):
     category_skill = models.ForeignKey(category, default=2, verbose_name="category", on_delete=models.CASCADE)
@@ -48,20 +26,21 @@ class dept_skill(models.Model):
     def __str__(self):
         return self.department_name_skill
 
-
 class program_entre(models.Model):
-    depart_name = models.ForeignKey(dept_entre, on_delete=models.CASCADE)
-    program_name = models.CharField(max_length=100, unique=True)
-    state = models.CharField(max_length=100, blank=True)
-    financial_year = models.CharField(max_length=10)
-    college_name = models.CharField(max_length=100)
-    no_of_participants = models.IntegerField(null=True)
-    address = AddressField()
+    depart_name_ent = models.ForeignKey(dept_entre, on_delete=models.CASCADE)
+    program_name_ent = models.CharField(max_length=100, unique=True)
+    state_ent = models.CharField(max_length=100, blank=True)
+    financial_year_ent = models.CharField(max_length=10)
+    college_name_ent = models.CharField(max_length=100)
+    no_of_participants_ent = models.IntegerField(null=True)
+    address_ent = program_address_entre()
     objects = models.Manager()
-
     def __str__(self):
         return self.program_name
 
+class program_address_entre(CompositeField):
+    location = models.CharField(max_length=50,unique=False)
+    city = models.CharField(max_length=40,unique=False)
 
 class program_skill(models.Model):
     depart_name_skill = models.ForeignKey(dept_entre, on_delete=models.CASCADE)
@@ -69,22 +48,27 @@ class program_skill(models.Model):
     financial_year_skill = financial_year = models.CharField(max_length=10)
     trade_skill = models.CharField(max_length=100, blank=True)
     no_of_participants_skill = models.IntegerField(null=True)
-    Address_skill = AddressField()
+    address_skill = program_address_skill()
     objects = models.Manager()
-
     def __str__(self):
         return self.program_name_skill
 
+class program_address_skill(CompositeField):
+    state = models.CharField(max_length=30)
+    location = models.CharField(max_length=50, unique=False)
+    city = models.CharField(max_length=40, unique=False)
 
 class participant_skill(models.Model):
     CATEGORY_CHOICES = ['General', 'OBC', 'SC', 'ST']
+    GENDER_CHOICES = ['Male', 'Female', 'Transgender']
+    MARITAL_STATUS_CHOICES = ['Single/Unmarried', 'Married', 'Widowed', 'Divorced', 'Separated', 'Not to be Disclosed']
     program_id = models.ForeignKey(program_skill, on_delete=models.CASCADE)
     participant_id_skill = models.IntegerField(primary_key=True, unique=True)
-    name_skill = NameField()
-    gender = models.CharField(max_length=25, blank=True)
+    name_skill = participant_name_skill()
+    gender = models.CharField(max_length=25, blank=True, choices=GENDER_CHOICES)
     dob = models.DateField()
     email = models.EmailField(max_length=25, blank=True, unique=True)
-    marital_status = models.CharField(max_length=25, blank=True)
+    marital_status = models.CharField(max_length=25, blank=True, choices=MARITAL_STATUS_CHOICES)
     fathers_name = models.CharField(max_length=25, blank=True)
     mothers_name = models.CharField(max_length=25, blank=True)
     religion = models.CharField(max_length=25, blank=True)
@@ -93,49 +77,59 @@ class participant_skill(models.Model):
     pa_same_as_ca = models.CharField(max_length=25, blank=True)
     training_status = models.CharField(max_length=25, blank=True)
     heard_about_us = models.CharField(max_length=25, blank=True)
-    Communication_address = ParticipantComAddField()
-    contact_number = CollectionField(item_type=int, collection_type=set, max_length = 10)
+    comm_address = participant_comm_address_skill()
+    disability = participant_disability_skill()
+    domicile = participant_domicile_skill()
+    participant_id = participant_id_skill()
+    mobile = participant_mobile_skill()
+    perm_address = participant_perm_address_skill()
+    participant_job_details = participant_job_details_skill()
 
+class participant_name_skill(CompositeField):
+    SALUTATION_CHOICES = ['Mr','Ms','Mrs','Mx'] 
+    salutation = models.CharField(max_length=25, blank=True, choices=SALUTATION_CHOICES)
+    FirstName = models.CharField(max_length=25, blank=True)
+    LastName = models.CharField(max_length=25, blank=True)
 
+class participant_comm_address_skill(CompositeField):
+    address_comm = models.CharField(max_length=100, blank=True)
+    state_comm = models.CharField(max_length=25, blank=True)
+    district_comm = models.CharField(max_length=25, blank=True)
+    pincode_comm = models.IntegerField()
+    city_comm = models.CharField(max_length=25, blank=True)
+    tehsil_comm = models.CharField(max_length=25, blank=True)
+    constituency_comm = models.CharField(max_length=25, blank=True)
 
-
-class participant_disability_skill(models.Model):
-    participant_id_skill = models.ForeignKey(participant_skill, on_delete=models.CASCADE)
-    disability_input = models.CharField(max_length=25, blank=True)
+class participant_disability_skill(CompositeField):
+    INPUT_CHOICES = ['Yes','No']
+    TYPE_CHOICES = ['Locomotor Disability','Leprosy Cured Person','Dwarfism','Acid Attack Victim','Blindness/Visual Impairment','Low-vision (Visual Impairment)','Deaf','Hard of Hearing','Speech and Language Disability','Intellectual Disability /Mental Retardation','Autism Spectrum Disorder','Specific Learning Disabilities','Mental Behavior-Mental Illness','Haemophilia','Thalassemia','Sickle Cell Disease','Deaf Blindness','Cerebral Palsy','Multiple Sclerosis','Muscular Dystrophy']
+    disability_input = models.CharField(max_length=25, blank=True, choices=INPUT_CHOICES)
     disability_type = models.CharField(max_length=25, blank=True)
 
-
-class participant_domicile_skill(models.Model):
-    participant_id_skill = models.ForeignKey(participant_skill, on_delete=models.CASCADE)
+class participant_domicile_skill(CompositeField):
     dom_state = models.CharField(max_length=25, blank=True)
     dom_district = models.CharField(max_length=25, blank=True)
 
-
-class participant_id_skill(models.Model):
-    participant_id_skill = models.ForeignKey(participant_skill, on_delete=models.CASCADE)
+class participant_id_skill(CompositeField):
     id_type = models.CharField(max_length=25, blank=True)
     alt_id_type = models.CharField(max_length=25, blank=True)
     aadhaar_ref_no = models.IntegerField(unique=True)
     id_no = models.IntegerField(unique=True)
 
-
-class participant_job_details_skill(models.Model):
-    participant_id_skill = models.ForeignKey(participant_skill, on_delete=models.CASCADE)
+class participant_job_details_skill(CompositeField):
+    EMPLOYED_CHOICES = ['Yes','No']
+    EMPLOYMENT_STATUS_CHOICES = ['Employed Through Registered Employer','Opted for Higher Studies','Self Employed','Up Skilled','Employed','NA','Employed at Training Partner','Employed at Other Firm']
     prev_exp_sector = models.CharField(max_length=25, blank=True)
     no_of_months = models.IntegerField()
-    employed = models.CharField(max_length=25, blank=True)
-    employment_status = models.CharField(max_length=25, blank=True, unique=False)
-    employment_details = models.CharField(max_length=50, blank=True)
+    employed = models.CharField(max_length=25, blank=True, choices=EMPLOYED_CHOICES)
+    employment_status = models.CharField(max_length=25, blank=True, unique=False, choices=EMPLOYMENT_STATUS_CHOICES)
+    employment_details = models.CharField(max_length=100, blank=True)
 
-
-class participant_mobile_skill(models.Model):
-    participant_id_skill = models.ForeignKey(participant_skill, on_delete=models.CASCADE)
+class participant_mobile_skill(CompositeField):
     country_code = models.IntegerField()
-    mobile_number = models.IntegerField(unique=True)
+    mobile_number = CollectionField(item_type=int, collection_type=set, max_length = 10)
 
-
-class participant_perm_address_skill(models.Model):
-    participant_id_skill = models.ForeignKey(participant_skill, on_delete=models.CASCADE)
+class participant_perm_address_skill(CompositeField):
     address_perm = models.CharField(max_length=100, blank=True)
     state_perm = models.CharField(max_length=25, blank=True, unique=False)
     district_perm = models.CharField(max_length=25, blank=True, unique=False)
@@ -144,9 +138,9 @@ class participant_perm_address_skill(models.Model):
     tehsil_perm = models.CharField(max_length=25, blank=True, unique=False)
     constituency_perm = models.CharField(max_length=25, blank=True, unique=False)
 
-
 class placement_skill(models.Model):
     participant_id_skill = models.ForeignKey(participant_skill, on_delete=models.CASCADE)
+    placement_id = placement_id_skill()
     course_name = models.CharField(max_length=50, blank=True, unique=False)
     placement_status = models.CharField(max_length=25, blank=True, unique=False)
     reason = models.CharField(max_length=25, blank=True, unique=False)
@@ -160,18 +154,15 @@ class placement_skill(models.Model):
     contact_person = models.CharField(max_length=25, blank=True, unique=False)
     contact_person_no = models.IntegerField()
 
-
-class placement_skill(models.Model):
-    participant_id_skill = models.ForeignKey(participant_skill, on_delete=models.CASCADE)
+class placement_id_skill(CompositeField):
     batch_id = models.IntegerField()
     rced_batch_id = models.IntegerField()
-
 
 class participant_entre(models.Model):
     CATEGORY_CHOICES = ['General', 'OBC', 'SC', 'ST']
     program_id = models.ForeignKey(program_entre, default=1, on_delete=models.CASCADE)
     participant_id_ent = models.IntegerField(primary_key=True, unique=True)
-    name_of_trainee = NameField()
+    name_of_trainee = models.CharField(max_length=50,unique=False)
     father_or_husband_name = models.CharField(max_length=50, unique=False)
     gender = models.CharField(max_length=10, unique=False)
     date_of_birth = models.DateField()
@@ -191,15 +182,21 @@ class participant_entre(models.Model):
     date_of_loan_release = models.DateField()
     commencement_date = models.DateField()
     no_of_persons_employed = models.IntegerField()
-    Address = AddressField()
-
+    email = models.CharField(item_type=char, collection_type=set, max_length = 50)
+    mobile_number = models.IntegerField(item_type=int, collection_type=set, max_length = 10)
+    address_entre = participant_address_entre()
+    participant_ID = participant_ID_entre() 
+    
 class participant_project_cost_entre(models.Model):
     Serial_number = models.ForeignKey(participant_entre, default=1, on_delete=models.CASCADE)
     CE = models.IntegerField(default=0)
     WC = models.IntegerField(default=0)
-
     @property
     def total(self):
         return self.CE + self.WC
 
-
+class participant_address_entre(CompositeField): 
+    serial_number=models.ForeignKey(participant_entre,default=1,on_delete=models.CASCADE)
+    location = models.CharField(max_length=100,unique=False)
+    city =  models.CharField(max_length=50,unique=False)
+    state =  models.CharField(max_length=40,unique=False)
